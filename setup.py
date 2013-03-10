b3a77fdbf626e1c73a20ed3d4629b2fab6521cc9
Sam Rushing <gitsam@rushing.nightmare.com>
2013-03-09 19:59:06 -0800

    beginnings of proper package setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- Mode: Python -*-
+
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+exts = [
+    Extension ('caesure.proto', ['caesure/proto.pyx']),
+    ]
+
+setup (
+    name             = 'caesure',
+    version          = '0.1',
+    description      = 'bitcoin node',
+    author           = "Sam Rushing",
+    packages         = ['caesure'],
+    ext_modules      = exts,
+    install_requires = ['cython>=0.18'],
+    license          = 'Simplified BSD',
+    cmdclass = {'build_ext': build_ext},
+    )
