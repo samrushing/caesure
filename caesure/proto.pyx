ee37b51a39c65ec0417f22ac938c41e70f2cf67e
Sam Rushing <gitsam@rushing.nightmare.com>
2013-03-09 20:00:26 -0800

    cython packet cracking

---
+++ caesure/proto.pyx
@@ -0,0 +1,294 @@
+# -*- Mode: Cython -*-
+
+import socket
+from socket import AF_INET6, inet_ntop
+from libc.stdint cimport uint64_t, uint32_t, uint16_t, uint8_t
+from cpython.bytes cimport PyBytes_FromStringAndSize
+
+cdef class pkt:
+    cdef bytes data
+    cdef unsigned char * d
+    cdef int len
+    cdef int pos
+
+    def __init__ (self, bytes data):
+        self.data = data
+        self.d = data
+        self.len = len(data)
+        self.pos = 0
+    
+    cdef need (self, int n):
+        if self.pos + n > self.len:
+            raise IndexError (self.len, self.pos +n)
+
+    cdef uint8_t u8 (self):
+        cdef uint8_t r
+        self.need (1)
+        r = self.d[self.pos]
+        self.pos += 1
+        return r
+
+    cdef uint16_t u16 (self):
+        cdef uint16_t r
+        self.need (2)
+        r = (self.d[self.pos+1] << 8) | self.d[self.pos] 
+        self.pos += 2
+        return r
+
+    cdef uint16_t net_u16 (self):
+        cdef uint16_t r
+        self.need (2)
+        r = (self.d[self.pos+0] << 8) | self.d[self.pos+1] 
+        self.pos += 2
+        return r
+
+    cdef uint32_t u32 (self):
+        cdef uint32_t r
+        self.need (4)
+        r = (  (self.d[self.pos+3] << 24)
+             | (self.d[self.pos+2] << 16)
+             | (self.d[self.pos+1] << 8)
+             | (self.d[self.pos] ) )
+        self.pos += 4
+        return r
+
+    cdef uint32_t u64 (self):
+        cdef uint64_t r = 0
+        cdef int i
+        self.need (8)
+        for i in range (7,-1,-1):
+            r <<= 8
+            r |= self.d[self.pos+i]
+        self.pos += 8
+        return r
+
+    cdef uint64_t unpack_var_int (self):
+        cdef uint8_t n0 = self.u8()
+        if n0 < 0xfd:
+            return n0
+        elif n0 == 0xfd:
+            return self.u16()
+        elif n0 == 0xfe:
+            return self.u32()
+        else:
+            return self.u64()
+
+    cdef bytes unpack_str (self, uint64_t n):
+        cdef bytes r
+        self.need (n)
+        r = self.d[self.pos:self.pos+n]
+        self.pos += n
+        return r
+
+    cdef bytes unpack_var_str (self):
+        cdef uint64_t n = self.unpack_var_int()
+        return self.unpack_str (n)
+
+    cdef unpack_net_addr (self):
+        cdef uint64_t services = self.u64()
+        cdef bytes addr = self.unpack_str (16)
+        cdef uint16_t port = self.net_u16()
+        addr = inet_ntop (AF_INET6, addr)
+        if addr.startswith ('::ffff:'):
+            addr = addr[7:]
+        return services, (addr, port)
+
+cdef class VERSION:
+    cdef public uint32_t version
+    cdef public uint64_t services
+    cdef public uint64_t timestamp
+    cdef public object me_addr
+    cdef public object you_addr
+    cdef public uint64_t nonce
+    cdef public bytes sub_version_num
+    cdef public uint32_t start_height
+    
+    def unpack (self, bytes data):
+        cdef pkt p = pkt (data)
+        self.version = p.u32()
+        self.services = p.u64()
+        self.timestamp = p.u64()
+        self.me_addr = p.unpack_net_addr()
+        self.you_addr = p.unpack_net_addr()
+        self.nonce = p.u64()
+        self.sub_version_num = p.unpack_var_str()
+        self.start_height = p.u32()
+
+    def dump (self, fout):
+        fout.write (
+            'VERSION {\n'
+            '  version=%d\n'
+            '  services=%d\n'
+            '  timestamp=%d\n'
+            '  me_addr=%r\n'
+            '  you_addr=%r\n'
+            '  nonce=%d\n'
+            '  sub_version_num=%r\n'
+            '  start_height=%d\n'
+            '  }\n' % (
+                self.version, self.services, self.timestamp, self.me_addr, self.you_addr, self.nonce, self.sub_version_num, self.start_height
+                )
+            )
+
+cdef class TX:
+    cdef public uint32_t version
+    cdef public uint32_t lock_time
+    cdef public list inputs
+    cdef public list outputs
+    cdef public bytes raw
+
+    cdef unpack_input (self, pkt p):
+        cdef bytes outpoint_hash = p.unpack_str (32)
+        cdef uint32_t outpoint_index = p.u32()
+        cdef uint64_t script_length = p.unpack_var_int()
+        cdef bytes script = p.unpack_str (script_length)
+        cdef uint32_t sequence = p.u32()
+        return ((outpoint_hash, outpoint_index), script, sequence)
+
+    cdef unpack_output (self, pkt p):
+        cdef uint64_t value = p.u64()
+        cdef uint64_t pk_script_length = p.unpack_var_int()
+        cdef bytes script = p.unpack_str (pk_script_length)
+        return (value, script)
+
+    cdef unpack0 (self, pkt p):
+        cdef uint64_t txin_count
+        cdef uint64_t txout_count
+        cdef int i
+        self.inputs = []
+        self.outputs = []
+        self.version = p.u32()
+        txin_count = p.unpack_var_int()
+        for i in range (txin_count):
+            self.inputs.append (self.unpack_input (p))
+        txout_count = p.unpack_var_int()
+        for i in range (txout_count):
+            self.outputs.append (self.unpack_output (p))
+        self.lock_time = p.u32()
+            
+    # unpack1 only exists to capture self.raw during block unpacking
+    cdef unpack1 (self, pkt p):
+        cdef int pos0 = p.pos
+        cdef int pos1
+        self.unpack0 (p)
+        pos1 = p.pos
+        self.raw = p.data[pos0:pos1]
+
+    def unpack (self, bytes data):
+        self.raw = data
+        return self.unpack0 (pkt (data))
+
+cdef char *hexdigits = "0123456789abcdef"
+
+cdef _hexify (bytes s):
+    cdef int slen = len (s)
+    cdef bytes o = PyBytes_FromStringAndSize (NULL, slen * 2)
+    cdef unsigned char * a = s
+    cdef unsigned char * b = o
+    cdef int i
+    for i in range (slen):
+        b[i*2]   = hexdigits[a[i] >> 4]
+        b[i*2+1] = hexdigits[a[i] & 0xf]
+    return o
+
+cdef _flip_hexify (bytes s):
+    cdef int slen = len (s)
+    cdef bytes o = PyBytes_FromStringAndSize (NULL, slen * 2)
+    cdef unsigned char * a = s
+    cdef unsigned char * b = o
+    cdef int i
+    cdef int p
+    for i in range (slen):
+        p = (2*slen)-((i+1)*2)
+        b[p]   = hexdigits[a[i] >> 4]
+        b[p+1] = hexdigits[a[i] & 0xf]
+    return o
+
+def hexify (bytes s, bint flip=False):
+    if flip:
+        return _flip_hexify (s)
+    else:
+        return _hexify (s)
+
+cdef class BLOCK:
+    cdef public uint32_t version
+    # XXX consider putting these here as char x[32]
+    cdef public bytes prev_block
+    cdef public bytes merkle_root
+    cdef public uint32_t timestamp
+    cdef public uint32_t bits    
+    cdef public uint32_t nonce
+    cdef public list transactions
+    cdef public bytes raw
+    
+    def make_TX (self):
+        return TX()
+
+    def unpack (self, bytes data):
+        cdef pkt p = pkt (data)
+        cdef uint64_t tx_count
+        cdef TX tx
+        self.raw = data
+        self.version = p.u32()
+        self.prev_block = _flip_hexify (p.unpack_str (32))
+        self.merkle_root = p.unpack_str (32)
+        self.timestamp = p.u32()
+        self.bits = p.u32()
+        self.nonce = p.u32()
+        tx_count = p.unpack_var_int()
+        self.transactions = []
+        cdef int i
+        for i in range (tx_count):
+            tx = self.make_TX()
+            tx.unpack1 (p)
+            self.transactions.append (tx)
+
+def unpack_block_header (bytes data):
+    cdef pkt p = pkt (data)
+    raw = data
+    version = p.u32()
+    prev_block = p.unpack_str (32)
+    merkle_root = p.unpack_str (32)
+    timestamp = p.u32()
+    bits = p.u32()
+    nonce = p.u32()
+    return (version, prev_block, merkle_root, timestamp, bits, nonce)
+
+def make_block (data):
+    b = BLOCK()
+    b.unpack (data)
+    return b
+
+def make_tx (data):
+    tx = TX()
+    tx.unpack (data)
+    return tx
+
+def unpack_version (data):
+    v = VERSION()
+    v.unpack (data)
+    return v
+
+def unpack_inv (data):
+    cdef pkt p = pkt (data)
+    cdef uint64_t count = p.unpack_var_int()
+    cdef list result = []
+    cdef int i
+    for i in range (count):
+        obj_id = p.u32()
+        obj_hash = p.unpack_str (32)
+        result.append ((obj_id, obj_hash))
+    return result
+
+def unpack_addr (data):
+    cdef pkt p = pkt (data)
+    cdef uint64_t count = p.unpack_var_int()
+    cdef list result = []
+    cdef int i
+    cdef uint32_t timestamp
+    for i in range (count):
+        result.append ((p.u32(), p.unpack_net_addr()))
+    return result
+        
+def unpack_getdata (data):
+    return unpack_inv (data)
