c01f8afed1fa47389c009e3f6a8df9a0d4891864
rushing <gitsam@rushing.nightmare.com>
2014-10-14 15:36:21 -0700

    ansi tricks hide here.

---
+++ caesure/ansi.py
@@ -0,0 +1,24 @@
+# -*- Mode: Python; indent-tabs-mode: nil -*-
+
+import coro
+
+W = coro.write_stderr
+
+# bright = 30 + n:
+#   0    1    2      3     4     5      6    7
+# Black Red Green Yellow Blue Magenta Cyan White
+
+def ansi (m, color):
+    return (('\x1b[1;%dm' % color) + m + '\x1b[0m')
+
+def WT (m):
+    "packet to (red-shifted)"
+    W (ansi (m, 31))
+
+def WF (m):
+    "packet from (blue-shifted)"
+    W (ansi (m, 34))
+
+def WY (m):
+    W (ansi (m, 33))
+
