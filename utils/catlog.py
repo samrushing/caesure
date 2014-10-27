3c6dcd99263ce6d45786375bbbda43686d3c7e47
Sam Rushing <gitsam@rushing.nightmare.com>
2014-10-26 20:27:20 -0700

    preparing for a move to the 'scripts' dir.

--- utils/catlog.py
+++ utils/catlog.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- Mode: Python -*-
 
 import argparse
@@ -13,7 +14,7 @@ class Sync:
         self.state = 0
         self.last = None
     def feed (self, ch):
-        sys.stderr.write ('[%d ch=%r]' % (self.state, ch))
+        #sys.stderr.write ('[%d ch=%r]' % (self.state, ch))
         if ch == self.magic[self.state]:
             self.state += 1
             if self.state == 4:
@@ -59,27 +60,41 @@ def frob(ob):
     else:
         return ob
 
-p = argparse.ArgumentParser()
-p.add_argument ('-b', '--big', action='store_true', help="show large strings", default=False)
-p.add_argument ('-bh', '--big-hex', action='store_true', help="show large strings in hex", default=False)
-p.add_argument ('-nb', '--no_blocks', action='store_true', help="elide 'block' packets", default=False)
-args = p.parse_args()
+def main():
+    global args
+    p = argparse.ArgumentParser()
+    p.add_argument ('-b', '--big', action='store_true', help="show large strings", default=False)
+    p.add_argument ('-bh', '--big-hex', action='store_true', help="show large strings in hex", default=False)
+    p.add_argument ('-nb', '--no_blocks', action='store_true', help="elide 'block' packets", default=False)
+    p.add_argument ('file', help="input file", nargs="?", metavar="FILE")
+    args = p.parse_args()
 
-s = Sync()
-s.resync (sys.stdin)
-
-while 1:
-    size, = struct.unpack ('>I', sys.stdin.read (4))
-    block = sys.stdin.read (size)
-    (timestamp, info), size = decode (block)
-    timestamp /= 1000000.0
-    if args.no_blocks and len(info) > 3 and info[2] == 'block':
-        pass
+    if args.file is None:
+        file = sys.stdin
     else:
-        info = [frob(x) for x in info]
-        print time.ctime (timestamp), info
-    magic = sys.stdin.read (4)
-    if not magic:
-        break
-    elif magic != Sync.magic:
-        s.resync (sys.stdin)
+        file = open (args.file, 'rb')
+
+    s = Sync()
+
+    s.resync (file)
+
+    while 1:
+        size, = struct.unpack ('>I', file.read (4))
+        block = file.read (size)
+        if len(block) != size:
+            break
+        (timestamp, info), size = decode (block)
+        timestamp /= 1000000.0
+        if args.no_blocks and len(info) > 3 and info[2] == 'block':
+            pass
+        else:
+            info = [frob(x) for x in info]
+            print time.ctime (timestamp), info
+        magic = file.read (4)
+        if not magic:
+            break
+        elif magic != Sync.magic:
+            s.resync (file)
+
+if __name__ == '__main__':
+    main()
