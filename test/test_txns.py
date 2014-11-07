144c19f46d87f00fb473388ddd98ec9ee929ab44
Sam Rushing <sam-github@rushing.nightmare.com>
2014-11-07 07:45:20 +0000

    collect failures into a file.  use argparse.

--- test/test_txns.py
+++ test/test_txns.py
@@ -4,6 +4,10 @@ import coro
 from caesure.bitcoin import KEY, TX
 from caesure.script import *
 
+import sys
+
+W = sys.stderr.write
+
 # XXX consider piping via stdin
 # $ lpython ../utils/find_multi.py 315000 316000 > multisig_tests.txt
 # $ lpython test_txns.py multisig_tests.txt
@@ -17,23 +21,54 @@ def test (lock_script, tx_raw, index, block_timestamp):
     tx.unpack (tx_raw)
     tx.verify (index, lock_script, block_timestamp)
     
-def test_all():
-    import sys
+class BadFile:
+    def __init__ (self, path):
+        self.bad_path = path + '.bad'
+        self.file = None
+    def write (self, line):
+        if self.file is None:
+            self.file = open (self.bad_path, 'wb')
+        self.file.write (line)
+        self.file.flush()
+    def close (self):
+        if self.file:
+            self.file.close()
+
+def test_all (args):
+
+    if args.debug:
+        verifying_machine.debug = True
+
     good = 0
     bad  = 0
     
-    for line in open (sys.argv[1], 'rb'):
+    fbad = BadFile (args.file)
+    for line in open (args.file, 'rb'):
+        if line.startswith ('#'):
+            continue
         lock_script, txraw, index, block_timestamp = line.split()
         lock_script = HD (lock_script)
         txraw = HD (txraw)
         index = int (index)
         block_timestamp = int (block_timestamp)
+        if args.debug:
+            W ('tx: %d %s\n' % (index, dhash (txraw)[::-1].encode ('hex'),))
         try:
             test (lock_script, txraw, index, block_timestamp)
             good += 1
         except:
-            print coro.compact_traceback()
+            W ('error: %r\n' % (coro.compact_traceback(),))
             bad += 1
-    print '%d good, %d bad' % (good, bad)
+            fbad.write (line)
+        if args.debug:
+            W ('hit <enter> to continue\n')
+            raw_input()
+    W ('%d good, %d bad\n' % (good, bad))
+    fbad.close()
 
-test_all()
+import argparse
+p = argparse.ArgumentParser (description='Run the VM over a set of test txn inputs')
+p.add_argument ('-d', '--debug', action='store_true', help='Have the VM print debug output', default=False)
+p.add_argument ('file', help='file of test cases, one per line.', default=False)
+args = p.parse_args()
+test_all (args)
