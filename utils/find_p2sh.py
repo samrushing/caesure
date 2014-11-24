8f30cdbf59f21c8846563273d0ce6208e5580b29
Sam Rushing <gitsam@rushing.nightmare.com>
2014-11-23 20:07:11 -0800

    new utxo.

--- utils/find_p2sh.py
+++ utils/find_p2sh.py
@@ -1,7 +1,7 @@
 # -*- Mode: Python -*-
 
 from scan_utxo import gen_utxo
-from caesure.script import parse_script, pprint_script, ScriptError, OPCODES
+from caesure.script import parse_script, pprint_script, ScriptError, OPCODES, PUSH_OP
 from caesure._script import ScriptError
 
 def is_p2sh (s):
@@ -10,17 +10,17 @@ def is_p2sh (s):
         and s[0] == (2, OPCODES.OP_HASH160)
         and s[2] == (2, OPCODES.OP_EQUAL)
         and s[1][0] == 0
+        and s[1][2] == PUSH_OP
         and len(s[1][1]) == 20
     )
 
-for txname, outputs in gen_utxo():
-    for (index, amt, script) in outputs:
-        try:
-            script = parse_script (script)
-            if is_p2sh (script):
-                print pprint_script (script)
-        except ScriptError:
-            # there are quite a few broken outpoint scripts in the utxo set.
-            pass
+for txname, index, amt, script in gen_utxo():
+    try:
+        script = parse_script (script)
+        if is_p2sh (script):
+            print pprint_script (script)
+    except ScriptError:
+        # there are quite a few broken outpoint scripts in the utxo set.
+        pass
         
     
