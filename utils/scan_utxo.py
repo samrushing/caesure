7ff2074c4c6216c1a203ad9fc5da4d7e6ccc1b60
Sam Rushing <gitsam@rushing.nightmare.com>
2014-11-23 20:06:58 -0800

    new utxo.

--- utils/scan_utxo.py
+++ utils/scan_utxo.py
@@ -10,7 +10,18 @@ def gen_utxo (path='/usr/local/caesure/utxo.bin'):
     [version, height, block_name, total, lost, fees, size] = info
     try:
         while 1:
-            # this is followed by ((txname,index), [(index, amt, script), ...])
+            # this is followed by (txname[:16], index, amt, script), ...
             yield df.read_object()
     except EOFError:
         f.close()
+
+if __name__ == '__main__':
+    import sys
+    W = sys.stderr.write
+    i = 0
+    for item in gen_utxo():
+        #print item
+        if i % 10000 == 0:
+            W ('.')
+        i += 1
+    W ('\ntotal: %d entries\n' % (i,))
