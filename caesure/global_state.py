46a482a19f019d1f080b4eccd5e321cb4dcbb9cc
Sam Rushing <sam-github@rushing.nightmare.com>
2014-11-24 18:26:17 -0800

    remove network params.

--- caesure/global_state.py
+++ caesure/global_state.py
@@ -7,7 +7,9 @@ from caesure.bitcoin import *
 # fill in more defaults.
 class GlobalState:
     # XXX put everything for switching to testnet here.
-    MAGIC = MAGIC
-    genesis_block_hash = genesis_block_hash
-    BITCOIN_PORT = BITCOIN_PORT
+    # XXX urgh, I think I want these in a different object.
+    #MAGIC = MAGIC
+    #genesis_block_hash = genesis_block_hash
+    #BITCOIN_PORT = BITCOIN_PORT
+    pass
 
